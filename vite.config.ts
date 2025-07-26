0b5268fa1c401bc110a90bfb8092c2a5fe4e84f4
shlomii <shlomii@mediaforce.com>
2025-07-26 22:34:19 +0300

    feat: Switch to GitHub Actions for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/xonix/',
   server: {
     host: "::",
     port: 8080,
